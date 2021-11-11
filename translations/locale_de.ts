e1c0d69a09c07ffde9747c769ce9aa3d5f8efd02
simonstuder <s.studer.s@gmail.com>
2021-11-11 03:20:18 +0100

    update translations

--- translations/locale_de.ts
+++ translations/locale_de.ts
@@ -2,62 +2,83 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
-    <name>ExportLettersNNumbers</name>
+    <name>ExportNumbersNLetters</name>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="491"/>
         <source>Preview of current settings on the right</source>
         <translation>Vorschau mit den gewählten Einstellungen</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="550"/>
         <source>Use sharps or flats</source>
         <translation>Verwende Kreuze oder Bes</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="564"/>
         <source>sharps</source>
         <translation>Kreuze</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="565"/>
         <source>flats</source>
         <translation>Bes</translation>
     </message>
     <message>
-        <source>Layout break creates newline</source>
-        <translation>Umbruch generiert eine neue Zeile</translation>
-    </message>
-    <message>
-        <source>Cancel</source>
-        <translation>Schliessen</translation>
-    </message>
-    <message>
-        <source>Save Output</source>
-        <translation>Speichern</translation>
-    </message>
-    <message>
-        <source>Output destination</source>
-        <translation>Speicherort</translation>
-    </message>
-    <message>
+        <location filename="../ExportNumbersNLetters.qml" line="583"/>
         <source>Output Format</source>
         <translation>Ausgabeformat</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="616"/>
         <source>Letters file suffix</source>
         <translation>Suffix für Buchstaben</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="632"/>
+        <location filename="../ExportNumbersNLetters.qml" line="874"/>
         <source>Letters</source>
         <translation>Buchstaben</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="647"/>
         <source>Numbers file suffix</source>
         <translation>Suffix für Zahlen</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="663"/>
+        <location filename="../ExportNumbersNLetters.qml" line="878"/>
         <source>Numbers</source>
         <translation>Zahlen</translation>
     </message>
     <message>
+        <location filename="../ExportNumbersNLetters.qml" line="676"/>
+        <source>Layout break creates newline</source>
+        <translation>Umbruch generiert eine neue Zeile</translation>
+    </message>
+    <message>
+        <location filename="../ExportNumbersNLetters.qml" line="690"/>
+        <source>Use logarithmic spacing method</source>
+        <translation>Verwende logarithmische Abstände</translation>
+    </message>
+    <message>
+        <location filename="../ExportNumbersNLetters.qml" line="704"/>
         <source>Spacing</source>
         <translation>Abstände</translation>
+    </message>
+    <message>
+        <location filename="../ExportNumbersNLetters.qml" line="743"/>
+        <source>Save Output</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <location filename="../ExportNumbersNLetters.qml" line="754"/>
+        <source>Cancel</source>
+        <translation>Schliessen</translation>
+    </message>
+    <message>
+        <location filename="../ExportNumbersNLetters.qml" line="821"/>
+        <source>Output destination</source>
+        <translation>Speicherort</translation>
     </message>
 </context>
 </TS>
